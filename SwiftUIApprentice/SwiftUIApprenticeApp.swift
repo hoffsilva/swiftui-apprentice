0d61daf381f510be9f0118cbc6ee1c91c7cfe0ca
Hoff Silva <hoff.henry@gmail.com>
2021-05-28 10:36:43 -0300

    Initial Commit

---
+++ SwiftUIApprentice/SwiftUIApprenticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApprenticeApp.swift
+//  SwiftUIApprentice
+//
+//  Created by Hoff Henry Pereira da Silva on 28/05/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApprenticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
